f0d036b3573d0e120e716925e6bc659b9ac439f4
Shahzaib Iqbal <Shahzebiqbalqambrani@gmail.com>
2022-11-24 16:53:13 +0500

    ready...!

--- src/main/java/com/joseph/chatapp/controller/MessageController.java
+++ src/main/java/com/joseph/chatapp/controller/MessageController.java
@@ -32,16 +32,17 @@ public class MessageController {
     }
 
     @PostMapping("/generateOTP")
-    public ResponseEntity<StatusDTO> generateOTP(MessageDTO messageDTO){
-
+    public ResponseEntity<StatusDTO> generateOTP(MessageDTO messageDTO) {
         int otp = otpService.generateOTP();
-        if (otp>0){
-            messageDTO.setMessageBody("Your OTP(One-Time-Password) is: "+otp);
-//            senderImpple.sendOTP(messageDTO);
-            return new ResponseEntity<>(new StatusDTO(HttpStatus.OK.value(),"success",otp),HttpStatus.OK);
+        if (messageDTO.getPhoneNumber() != null) {
+            if (otp > 0) {
+                messageDTO.setMessageBody("Your OTP(One-Time-Password) is: " + otp);
+                senderImpple.sendOTP(messageDTO);
+                return new ResponseEntity<>(new StatusDTO(HttpStatus.OK.value(), "success", otp), HttpStatus.OK);
+            }
+            return new ResponseEntity<>(new StatusDTO(HttpStatus.OK.value(), "Failed", "Something went wrong"), HttpStatus.OK);
         }
-        return new ResponseEntity<>(new StatusDTO(HttpStatus.OK.value(),"Failed","Something went wrong"),HttpStatus.OK);
-
+        return new ResponseEntity<>(new StatusDTO(HttpStatus.OK.value(), "Failed", "Phone number cannot be null...!"), HttpStatus.OK);
     }
 
 
@@ -51,11 +52,11 @@ public class MessageController {
     @ResponseBody
     public String handleSmsWebhook(
             @RequestParam("From") String from,
-            @RequestParam("Body") String body){
+            @RequestParam("Body") String body) {
 
         System.out.println("reply sent successfully");
 
-        int thisMessageCount = messageCounts.compute(from, (k,v) -> (v == null) ? 1 : v+1);
+        int thisMessageCount = messageCounts.compute(from, (k, v) -> (v == null) ? 1 : v + 1);
 
         String plural = (thisMessageCount > 1) ? "messages" : "message";
         String message = String.format(
@@ -66,14 +67,16 @@ public class MessageController {
                 .message(new Message.Builder(message).build())
                 .build().toXml();
     }
+
     @PostMapping(value = "/sms")
-    public ResponseEntity<StatusDTO> message(MessageDTO messageDTO){
-        if (messageDTO.getPhoneNumber()!=null){
+    public ResponseEntity<StatusDTO> message(MessageDTO messageDTO) {
+        if (messageDTO.getPhoneNumber() != null) {
             senderImpple.sendSms(messageDTO);
-            return new ResponseEntity<>(new StatusDTO(HttpStatus.OK.value(), "Success","The Message Has Been Sent Successfully "),HttpStatus.OK);
-        }else{
-            return new ResponseEntity<>(new StatusDTO(HttpStatus.OK.value(), "Failed","The Message Could Not Send..!"),HttpStatus.OK);
+            return new ResponseEntity<>(new StatusDTO(HttpStatus.OK.value(), "Success", "The Message Has Been Sent Successfully "), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(new StatusDTO(HttpStatus.OK.value(), "Failed", "The Message Could Not Send..!"), HttpStatus.OK);
         }
+
     }
 
 }
